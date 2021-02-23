368e076aecc03c31ea0163d46f6e7d031adfede1
Maher <maher91syr@gmail.com>
2021-02-23 13:46:22 +0900

    Creating new files

---
+++ src/user/pages/Users.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import UsersList from "../components/UsersList";
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max",
+      image: "https://lifeabroad.jp/pics/mail_service/envelope.png",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
